8d054d8e1c0f46aa13eca942311d324a78168e62
Alpsencer <39705711+Alpsencer68@users.noreply.github.com>
2022-06-30 04:29:58 +0300

    Added some basic string scraping for travels containing only one train (direct travel)

---
+++ Ticket.py
@@ -0,0 +1,12 @@
+class Ticket:
+
+    def __init__(self, fro, to, start_date, start_time, end_date, end_time, price, av_set_num):
+        self.fro = fro
+        self.to = to
+        self.start_date = start_date
+        self.start_time = start_time
+        self.end_date = end_date
+        self.end_time = end_time
+        self.price = price
+        self.seat = av_set_num
+
--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import re
 # SPATH = "/usr/bin/safaridriver
 # FPATH = "/Users/alpsencer/Desktop/SeleniumDrivers/geckodriver"
 # os.environ['PATH'] += "/Users/alpsencer/Desktop/SeleniumDrivers/"
+from Ticket import Ticket
+
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 #driver = webdriver.Safari()
 # Get website content
@@ -38,10 +40,61 @@ driver.execute_script("arguments[0].click();", searchButton)
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//tbody[@id='mainTabView:gidisSeferTablosu_data']")))
 
 table = driver.find_element(By.XPATH, "//tbody[@id='mainTabView:gidisSeferTablosu_data']")
+text = table.text.strip()
 
-trainList = table.text.split("seç")
-for i in trainList:
-    print(re.findall("(\d+)", i))
-print(trainList)
+print(table.text)
+""" (Example text)
+07.07.2022
+09:30
+8s:22dk
+1 Aktarma 07.07.2022
+17:52
+i
+YHT : ANKARA-İSTANBUL : ( Ankara Gar - Eskişehir ) 09.30 - 10.56
+Standart
+Esnek
+i
+ANAHAT : EGE EKSPRESİ : ( Eskişehir - Balıkesir ) 12.10 - 17.52
+Standart
+Esnek
+2+2 Pulman (Ekonomi) (9)
+
+2+1 Pulman (1. Mevki) (1)
+
+₺ 158,00
+Seç
+
+07.07.2022
+21:00
+8s:39dk
+08.07.2022
+05:39
+i
+ANAHAT : İZMİR MAVİ
+Standart
+Esnek
+2+1 Pulman (1. Mevki) (0)
+
+₺ 121,00
+Seç
+"""
+
+trains = table.text.split("Seç")
+train_matrix = []
 
 
+for i in range(len(trains)):
+    train_matrix.append(list(filter(None, trains[i].split("\n"))))  # remove empty strings "" by using filter method
+train_matrix = list(filter(None, train_matrix))  # remove empty lists []
+
+print(train_matrix)
+ticket_list = []
+for train in train_matrix:
+    if re.match("[0-9] Aktarma", train[3]):  # if it is an indirect travel
+        pass
+    else:
+        ticket_list.append(Ticket("Ankara Gar", "Balıkesir", train[0], train[1], train[3], train[4], int(train[-1][2:-3]), int(train[-2][-2])))
+
+
+for ticket in ticket_list:
+    print(ticket.start_date)
